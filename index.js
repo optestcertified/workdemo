2e3d0315766479c22d34ece3f56750dec17e8ec1
opy <140424822+optestcertified@users.noreply.github.com>
2024-01-15 15:24:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api', (req, res) => {
+  res.send('It is a great day!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
